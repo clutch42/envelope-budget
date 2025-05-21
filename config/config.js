612a01cbfc1f55fc345c4bce2ec3f8f223d6fe36
Brian Engel <clutch_42@yahoo.com>
2025-05-20 22:19:45 -0500

    changed connfig.js

--- config/config.js
+++ config/config.js
@@ -18,8 +18,14 @@ module.exports = {
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
-    database: 'envelope_budget_prod',
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT
+    dialect: process.env.DB_DIALECT,
+    dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Allow self-signed Render cert
+      },
+    },
   }
 };
